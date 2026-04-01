1f273538288a439dded1fa8b4bbcfe5e5a4892db
olivia15023 <olivia-larisa.bako@student.upt.ro>
2026-04-01 17:24:59 +0300

    SM-2 Implemented the Student model class

---
+++ src/main/java/loose/oose/fis/lab/student/manager/Student.java
@@ -0,0 +1,70 @@
+package loose.oose.fis.lab.student.manager;
+
+import java.util.Objects;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(String lastName, String firstName, int age, double meanGrade) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+        this.meanGrade = meanGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", meanGrade=" + meanGrade +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Student student)) return false;
+        return age == student.age && Double.compare(meanGrade, student.meanGrade) == 0 && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age, meanGrade);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMeanGrade() {
+        return meanGrade;
+    }
+
+    public void setMeanGrade(double meanGrade) {
+        this.meanGrade = meanGrade;
+    }
+}
